5988071f02b05af227d64756bc4e6f69bb518da1
Karol Kochański <karolkochanski@MacBook-Pro.local>
2019-07-26 00:33:14 +0200

    Add project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+    state = {
+        data: {}
+    }
 
-export default App;
+    componentDidMount() {
+        var authOptions = {
+            method: 'GET',
+            url: 'https://airapi.airly.eu/v2/meta/indexes',
+            headers: {
+                'Accept': 'application/json',
+                'apikey': 'sAR15nUCNUooLodxOzUdvRzFZKvbVEh3'
+            },
+            json: true
+        };
+
+        axios(authOptions)
+        .then(function(response){
+            console.log(response.data);
+            console.log(response.status);
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+    }
+
+    render(){
+        return (
+            <div className="App">
+              <header className="App-header">
+
+
+
+              </header>
+            </div>
+        );
+    }
+}
